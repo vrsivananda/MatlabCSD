939d19db21a858cc8eaf0e9c88bac28108b664af
CML2 <cml2@CML2s-iMac.local>
2018-05-15 13:44:00 -0700

    Truncated

--- startCSD.m
+++ startCSD.m
@@ -1,45 +1,48 @@
-function output = startCSD(coherence, binaryChoice, confidence)
+function output = startCSD(stimulusIntensity, confidence)
 
     opts = optimset('fminsearch');
     opts.TolX = 1.e-4;
     opts.TolFun = 1.e-4;
     
+    % Convert confidence to binary
+    binaryChoice = confidence >= 0.5;
+    
     % ==============================================
     % Step B:
 
-    %This is the Psychometric function that we will use
+    % This is the Psychometric function that we will use
     disp('-------------------------------------');
     disp('Psychometric function on binomial data with 0 guess and 0 lapse rates:');
     disp('    mean      slope');
-    %Fitting to binary forced choice data:
-    parameters2 = psychometricFit2Parameters(coherence,binaryChoice,0,0,0);
-    disp(parameters2);
+    % Fitting to binary forced choice data:
+    parametersPsychometricFn = psychometricFit2Parameters(stimulusIntensity,binaryChoice);
+    disp(parametersPsychometricFn);
 
-    %Store what we need in variables
-    mu = parameters2(1);
-    slope = parameters2(2);
+    % Store what we need in variables
+    mu = parametersPsychometricFn(1);
+    slope = parametersPsychometricFn(2);
     sigma = 1/slope;
 
     % ==============================================
     % Step C:
 
-    %This is the function that we will use
+    % This is the function that we will use
     disp('-------------------------------------');
     disp('Confidence function with only k as the free parameter');
     disp('    scalar');
-    parameters4 = psychometricFit1Parameter(coherence,confidence,mu,slope,0,0,0);
-    disp(parameters4);
+    parametersConfidenceFn = psychometricFit1Parameter(stimulusIntensity,confidence,mu,slope);
+    disp(parametersConfidenceFn);
 
-    k = parameters4;
+    k = parametersConfidenceFn;
 
     % ==============================================
     % Step D:
 
-    %Preallocate for matrix to hold lower bin limit (col 1) and uper bin limit
-    %(col 2)
+    % Preallocate for matrix to hold lower bin limit (col 1) and upper bin limit
+    % (col 2)
     binLimits = nan(length(confidence),2);
 
-    %Fill in the bin
+    % Fill in the bin
     binLimits(:,1) = confidence(:,1) - 0.005;
     binLimits(:,2) = confidence(:,1) + 0.005;
 
@@ -47,7 +50,7 @@ function output = startCSD(coherence, binaryChoice, confidence)
     % ==============================================
     % Step E:
 
-    %Assignment has been done above in step C.
+    % Assignment has been done above in steps B and C.
     % mu sigma k
 
     %The initial guessing values for fmincon, to be varied
@@ -56,16 +59,16 @@ function output = startCSD(coherence, binaryChoice, confidence)
     % ==============================================
     % Steps F to H:
 
-    %Create an anonymous function to be used in fmincon
-    %This gets us around the problem where some variables need to be fixed (the
-    %original mu, sigma,and k) and others need to be changed to be optimized
-    %(those in x0).
-    f = @(x) stepsFtoH(x,confidence,binLimits,coherence);
+    % Create an anonymous function to be used in fmincon
+    % This gets us around the problem where some variables need to be fixed (the
+    % original mu, sigma,and k) and others need to be changed to be optimized
+    % (those in x0).
+    f = @(x) stepsFtoH(x,confidence,binLimits,stimulusIntensity);
 
 
     % ==============================================
     % Step I
-    %[finalX,fval, exitflag, output, lambda, grad, hessian] = fmincon(f,x0,[],[],[],[],[-0.5 0 0],[0.5 3 30]);
+    % [finalX,fval, exitflag, output, lambda, grad, hessian] = fmincon(f,x0,[],[],[],[],[-0.5 0 0],[0.5 3 30]);
     finalX = fminsearchbnd(f,x0,[-0.5 0 0],[0.5,3,10],opts);
 
     finalMu = finalX(1);
--- stepsFtoH.m
+++ stepsFtoH.m
@@ -1,4 +1,4 @@
-function output = stepsFtoH(x,confidence,binLimits, coherence)
+function output = stepsFtoH(x, confidence, binLimits, coherence)
     
     guessMu = x(1);
     guessSigma = x(2);
@@ -7,12 +7,12 @@ function output = stepsFtoH(x,confidence,binLimits, coherence)
     % ==============================================
     % Step F: 
 
-    %Preallocate matrix to hold lower limit (col 1) and upper limit (col 2) on
-    %the decision variable (Xj lower and Xj upper)
-    decisionVariableLimits = nan(length(confidence),2);
+    % Preallocate matrix to hold lower limit (col 1) and upper limit (col 2) on
+    % the decision variable (Xj lower and Xj upper)
+    decisionVariableLimits = nan(length(confidence), 2);
 
-    %Use a loop to calculate the lower limt and upper limit of decision
-    %variables
+    % Use a loop to calculate the lower limit and upper limit of decision
+    % variables
     for i = 1:length(confidence)
 
         % Make sure that all values are > 0 and < 1. If they are out of the range, pull
@@ -20,67 +20,53 @@ function output = stepsFtoH(x,confidence,binLimits, coherence)
         binLimits(binLimits < 0) = 0.0000000000000001;
         binLimits(binLimits > 1) = 0.9999999999999999;
         
-        
-        %[askBrian] Is the below guessMu, or just set to 0? (Set to 0 in
-        %cookbook, but maybe that's just for the first round. Because we
-        %need it to vary when calculating maximum log likelihood, it makes
-        %sense to make it vary. But even so, is it varied here or in Step G
-        %below? (Makes sense to not do it there since it is for the "fitted
-        %psychometric function"))
         % Lower limits
-       decisionVariableLimits(i,1) = inverseCumulativeGaussian([0, guessK*guessSigma],binLimits(i,1)); 
-       % Upper limits
-       decisionVariableLimits(i,2) = inverseCumulativeGaussian([0, guessK*guessSigma],binLimits(i,2));
+        decisionVariableLimits(i,1) = inverseCumulativeGaussian([0, guessK*guessSigma], binLimits(i,1)); 
+        % Upper limits
+        decisionVariableLimits(i,2) = inverseCumulativeGaussian([0, guessK*guessSigma], binLimits(i,2));
 
-       %[askBrian] Do we need this bound? (Probably not)
-       %Values that are < -1 or > 1 are meaningless (i.e. coherence cannot be
-       %less than -1 or more than 1)
-       %If they are outside that range, then make them -1 or 1
-       %decisionVariableLimits(decisionVariableLimits < -1) = -1;
-       %decisionVariableLimits(decisionVariableLimits > 1) = 1;
     end
-
 
     % ==============================================
     % Step G: 
 
 
-    %Calculate the probability of the specific confidence probability judgment
-    %(Pj)
-    %(Sj is the stimulus intensity/coherence at trial j)
+    % Calculate the probability of the specific confidence probability judgment
+    % (Pj)
+    % (Sj is the stimulus intensity/coherence at trial j)
 
-    %Preallocate matrix to hold the confidence probability judgments for each
-    %trial
+    % Preallocate matrix to hold the confidence probability judgments for each
+    % trial
     likelihood = nan(length(confidence),1);
 
 
-    %Make a loop to go through each trial and calculate the
-    %confidenceProbabilityJudgment (Pj)
+    % Make a loop to go through each trial and calculate the
+    % confidenceProbabilityJudgment (Pj)
     for i = 1:length(confidence)
 
-        %Sj is the stimulus intensity/coherence at the current trial
+        % Sj is the stimulus intensity/coherence at the current trial
         Sj = coherence(i);
 
-        %Place lower and upper bounds of the confidence probability judgment
-        %into variables for easy handling
+        % Place lower and upper bounds of the confidence probability judgment
+        % into variables for easy handling
         XjLower = decisionVariableLimits(i,1);
         XjUpper = decisionVariableLimits(i,2);
 
-        %Calculate the upper and lower bound probabilities
+        % Calculate the upper and lower bound probabilities
         UpperLimitProbability = cumulativeGaussian(XjUpper,[Sj+guessMu, guessSigma]);
         LowerLimitProbability = cumulativeGaussian(XjLower,[Sj+guessMu, guessSigma]);
 
-        %Calculate the probability for that interval and store it in the
-        %likelihood array
+        % Calculate the probability for that interval and store it in the
+        % likelihood array
         likelihood(i,1) = UpperLimitProbability - LowerLimitProbability;
 
     end
     
     logLikelihood = log(likelihood);
     
-    %Since the fmincon seeks to minimize, but we want the logLikelihood 
-    %to be maximized, we take the negative of the logLikelihood as the
-    %output (to be minimized by fmincon)
+    % Since the fmincon seeks to minimize, but we want the logLikelihood 
+    % to be maximized, we take the negative of the logLikelihood as the
+    % output (to be minimized by fmincon)
     output = sum(-logLikelihood);
 
 end
